20b830279c43046c051e472c42fb8683e9d23404
Alex Burka <aburka@seas.upenn.edu>
2013-09-07 12:32:19 -0400

    eclipse app regsiters and parses json

--- eclipse/src/edu/upenn/seas/pennapps/dumbledore/pollio/MainActivity.java
+++ eclipse/src/edu/upenn/seas/pennapps/dumbledore/pollio/MainActivity.java
@@ -1,5 +1,15 @@
 package edu.upenn.seas.pennapps.dumbledore.pollio;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -27,5 +37,6 @@ public class MainActivity extends Activity {
         Toast.makeText(getApplicationContext(), "button!", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, DemoActivity.class));
     }
+    
     
 }
