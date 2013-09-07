91478bbadb8c8618f4c5bc8c66e38960e9712506
steven <steven@steven-NBLBX.(none)>
2013-09-07 16:50:28 -0400

    adjustments to request params

--- django/django_server/pollio/models.py
+++ django/django_server/pollio/models.py
@@ -5,13 +5,21 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     registration_id = models.CharField(max_length=1024)
+    def __unicode__(self):
+        return self.name
 
 class Poll(models.Model):
-#    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, related_name='owner')
     question = models.CharField(max_length=200)
+    pollees = models.ManyToManyField(User, related_name='pollees')
     pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.question
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
+    backers = models.ManyToManyField(User, related_name='backers')
     votes = models.IntegerField()
+    def __unicode__(self):
+        return self.choice
