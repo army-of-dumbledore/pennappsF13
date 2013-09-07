71ed1a2abb578cbf02d9600496238e07650a679d
alexsalz1 <amsalz328@gmail.com>
2013-09-07 19:20:35 -0400

    salz wrote some stuff

---
+++ PollioProject/Pollio/src/main/java/edu/upenn/cis/pennapps/pollio/BinaryRequest.java
@@ -0,0 +1,14 @@
+package edu.upenn.cis.pennapps.pollio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alexsalz1 on 9/7/13.
+ */
+public class BinaryRequest extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.binary_request);
+    }
+}
