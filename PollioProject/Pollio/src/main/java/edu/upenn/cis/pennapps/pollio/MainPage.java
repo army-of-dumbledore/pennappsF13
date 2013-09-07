509d6b6ff25055627e4b49ff206bd9b7f668dff4
alexsalz1 <amsalz328@gmail.com>
2013-09-07 13:07:45 -0400

    added new layouts

---
+++ PollioProject/Pollio/src/main/java/edu/upenn/cis/pennapps/pollio/MainPage.java
@@ -0,0 +1,14 @@
+package edu.upenn.cis.pennapps.pollio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alexsalz1 on 9/7/13.
+ */
+public class MainPage extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
