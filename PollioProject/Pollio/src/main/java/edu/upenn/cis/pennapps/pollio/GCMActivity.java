b0306049008ea53d0d44945be855a84e29ff7eff
Alex Burka <aburka@seas.upenn.edu>
2013-09-07 03:59:24 -0400

    eclipse project which doesn't crash

---
+++ PollioProject/Pollio/src/main/java/edu/upenn/cis/pennapps/pollio/GCMActivity.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis.pennapps.pollio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alex on 9/7/13.
+ */
+public class GCMActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
