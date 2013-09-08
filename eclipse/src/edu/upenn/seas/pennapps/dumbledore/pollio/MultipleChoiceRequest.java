3f573702de004b01ff36c4e013f78f77fc65b83f
Alex Burka <aburka@seas.upenn.edu>
2013-09-07 23:39:44 -0400

    MC poll submission

--- eclipse/src/edu/upenn/seas/pennapps/dumbledore/pollio/MultipleChoiceRequest.java
+++ eclipse/src/edu/upenn/seas/pennapps/dumbledore/pollio/MultipleChoiceRequest.java
@@ -8,12 +8,19 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 /**
  * Created by alexsalz1 on 9/7/13.
  */
 public class MultipleChoiceRequest extends Activity {
+	
+	private String pollid;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multiple_choice_request);
@@ -27,6 +34,7 @@ public class MultipleChoiceRequest extends Activity {
         		          texts = new ArrayList<String>();
         
         try {
+        	pollid = jsono.getString("poll_id");
 			question = jsono.getString("question");
 			JSONArray choices = jsono.getJSONArray("choices");
 			for(int i=0; i<choices.length();i++){
@@ -41,5 +49,35 @@ public class MultipleChoiceRequest extends Activity {
         
         //vote button stuffs
         
+    }
+    
+    public void submit_poll(View view) {
+    	new AsyncTask<Void, Void, Boolean>() {
+    		@Override
+    		protected Boolean doInBackground(Void... params) {
+    			RadioGroup group = (RadioGroup) findViewById(R.id.mcradiogroup);
+    			int choiceid = group.getCheckedRadioButtonId();
+    			JSONObject json = InternetUtils.json_request("http://" + getResources().getString(R.string.server)+ "/polls/submit_vote",
+    					 									 "user_id", Utils.getUserId(getApplicationContext()),
+    					 									 "poll_id", pollid,
+    					 									 "choice_id", Integer.toString(choiceid));
+    			try {
+    				return json.getInt("success") == 0;
+    			} catch (JSONException e) {
+    				return false;
+    			}
+    		}
+    		
+    		@Override
+    		protected void onPostExecute(Boolean success) {
+    			if (success.booleanValue()) {
+    				Toast.makeText(getApplicationContext(), "Poll submission successful!", Toast.LENGTH_SHORT).show();
+    			} else {
+    				Toast.makeText(getApplicationContext(), "Poll submission failed :(", Toast.LENGTH_SHORT).show();
+    			}
+    		}
+    	}.execute(null, null, null);
+    	
+    	finish();
     }
 }
