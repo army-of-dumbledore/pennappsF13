9c51b2195b8c653b945ea28a4af66003e7a7968f
joe <josephjtrovato@gmail.com>
2013-09-07 23:13:40 -0400

    request stuffs

--- eclipse/src/edu/upenn/seas/pennapps/dumbledore/pollio/MultipleChoiceRequest.java
+++ eclipse/src/edu/upenn/seas/pennapps/dumbledore/pollio/MultipleChoiceRequest.java
@@ -1,6 +1,13 @@
 package edu.upenn.seas.pennapps.dumbledore.pollio;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -10,5 +17,29 @@ public class MultipleChoiceRequest extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multiple_choice_request);
+        
+        Intent intent  = getIntent();
+        Bundle extras = intent.getExtras();
+        JSONObject jsono = (JSONObject)extras.get("json");
+        
+        String question = "ERROR";
+        ArrayList<String> ids = new ArrayList<String>(),
+        		          texts = new ArrayList<String>();
+        
+        try {
+			question = jsono.getString("question");
+			JSONArray choices = jsono.getJSONArray("choices");
+			for(int i=0; i<choices.length();i++){
+				JSONObject item = choices.getJSONObject(i);
+				ids.add(item.getString("id"));
+				texts.add(item.getString("text"));
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //vote button stuffs
+        
     }
 }
